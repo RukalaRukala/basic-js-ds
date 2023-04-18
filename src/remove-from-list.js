880a9972a1f92de3b1e0596f3de761eb3da5b0fb
Mikalai Rusakovich <kolyos83@mail.ru>
2023-04-18 03:03:33 +0300

    feat:remove-from-list

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -22,9 +22,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+  if (l == null) {return l;}
+  while (l.value == k) {l = l.next;}
+  thisNode = l;
+  nextNode = thisNode.next;
+  while (thisNode && thisNode.value !== k) {
+    last = thisNode,
+    thisNode = thisNode.next;
+  }
+  if (last && thisNode.value === k) {
+    last.next = thisNode.next;
+  }
+  return l;
 }
 
 module.exports = {
